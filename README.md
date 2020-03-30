d9f1218883a721ad07f4493224b194915fb8b980
Adam Bujak <dmbujak3@gmail.com>
2020-03-30 06:09:03 -0400

    Initial Commit - blinky working on MSP432

---
+++ README.md
@@ -0,0 +1,14 @@
+FALCON
+
+# An attempt at making drone
+
+# Ideas
+ - Big drone to carry other small ones
+
+# ToDo
+ - Figure out a way to drive BLDC motor 
+ 
+# Documentation
+
+## Project Build Requirements
+ - Must include TI SDK at development/system/msp_sdk
---
+++ development/applications/drone/src/main.c
@@ -0,0 +1,82 @@
+
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+/* Standard Includes */
+#include <stdint.h>
+#include <stdbool.h>
+
+//![Simple Timer_A Config]
+/* Timer_A PWM Configuration Parameter */
+Timer_A_PWMConfig pwmConfig =
+{
+        TIMER_A_CLOCKSOURCE_SMCLK,
+        TIMER_A_CLOCKSOURCE_DIVIDER_1,
+        32000,
+        TIMER_A_CAPTURECOMPARE_REGISTER_1,
+        TIMER_A_OUTPUTMODE_RESET_SET,
+        3200
+};
+//![Simple Timer_A Config]
+
+int main(void)
+{
+    /* Halting the watchdog */
+    MAP_WDT_A_holdTimer();
+
+    //![Simple Timer_A Example]
+    /* Setting MCLK to REFO at 128Khz for LF mode
+     * Setting SMCLK to 64Khz */
+    MAP_CS_setReferenceOscillatorFrequency(CS_REFO_128KHZ);
+    MAP_CS_initClockSignal(CS_MCLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_1);
+    MAP_CS_initClockSignal(CS_SMCLK, CS_REFOCLK_SELECT, CS_CLOCK_DIVIDER_2);
+    MAP_PCM_setPowerState(PCM_AM_LF_VCORE0);
+
+    /* Configuring GPIO2.4 as peripheral output for PWM  and P6.7 for button
+     * interrupt */
+    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN4,
+            GPIO_PRIMARY_MODULE_FUNCTION);
+    MAP_GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, GPIO_PIN1);
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1, GPIO_PIN1);
+    MAP_GPIO_enableInterrupt(GPIO_PORT_P1, GPIO_PIN1);
+
+    /* Configuring Timer_A to have a period of approximately 500ms and
+     * an initial duty cycle of 10% of that (3200 ticks)  */
+    MAP_Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfig);
+    //![Simple Timer_A Example]
+
+    /* Enabling interrupts and starting the watchdog timer */
+    MAP_Interrupt_enableInterrupt(INT_PORT1);
+    MAP_Interrupt_enableSleepOnIsrExit();
+    MAP_Interrupt_enableMaster();
+
+    /* Sleeping when not in use */
+    while (1)
+    {
+        MAP_PCM_gotoLPM0();
+    }
+}
+
+/* Port1 ISR - This ISR will progressively step up the duty cycle of the PWM
+ * on a button press
+ */
+void PORT1_IRQHandler(void)
+{
+    uint32_t status = MAP_GPIO_getEnabledInterruptStatus(GPIO_PORT_P1);
+    MAP_GPIO_clearInterruptFlag(GPIO_PORT_P1, status);
+
+    if (status & GPIO_PIN1)
+    {
+        if(pwmConfig.dutyCycle == 28800)
+            pwmConfig.dutyCycle = 3200;
+        else
+            pwmConfig.dutyCycle += 3200;
+
+        MAP_Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfig);
+    }
+}
+
+int _mainCRTStartup (void)
+{
+  main();
+  return 1;
+}
