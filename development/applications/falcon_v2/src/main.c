b7bb81550ae177b56754dfd31096a6f6c111ebfd
Adam Bujak <adam@whisper.ai>
2020-10-05 00:32:25 -0700

    Move bringup code to bsp

--- development/applications/falcon_v2/src/main.c
+++ development/applications/falcon_v2/src/main.c
@@ -1,7 +1,6 @@
+#include "bsp.h"
 #include "falcon_common.h"
 
-#include "stm32f4xx_hal.h"
-#include "stm32412g_discovery.h"
 #include "leds.h"
 #include "logger.h"
 
@@ -12,17 +11,9 @@
 
 static void SystemClock_Config(void);
 
-void vApplicationTickHook(void)
-{
-  HAL_IncTick();
-}
-
 int main(void)
 {
-  HAL_Init();
-
-  SystemClock_Config();
-
+  bsp_board_bringup();
   leds_task_setup();
   logger_task_setup();
 
@@ -50,60 +41,3 @@ int main(void)
   /* Should never reach here */
   while (1);
 }
-
-static void SystemClock_Config(void)
-{
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  HAL_StatusTypeDef  ret = HAL_OK;
-
-  /* Enable Power Control clock */
-  __HAL_RCC_PWR_CLK_ENABLE();
-
-  /* The voltage scaling allows optimizing the power consumption when the device is
-     clocked below the maximum system frequency, to update the voltage scaling value
-     regarding system frequency refer to product datasheet.  */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-  /* Enable HSE Oscillator and activate PLL with HSE as source */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM       = 8;
-  RCC_OscInitStruct.PLL.PLLN       = 200;
-  RCC_OscInitStruct.PLL.PLLP       = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ       = 7;
-  RCC_OscInitStruct.PLL.PLLR       = 2;
-
-  ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
-  if (ret != HAL_OK) {
-    while (1);
-  }
-
-  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
-     clocks dividers */
-  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-  RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3);
-  if (ret != HAL_OK) {
-    while (1);
-  }
-}
-
-void error_handler(void)
-{
-  while (1);
-}
-
-#ifdef USE_FULL_ASSERT
-
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  while (1);
-}
-#endif
