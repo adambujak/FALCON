db93e626182f617c53644e91bcf4462485e64be5
Adam Bujak <adam@whisper.ai>
2020-10-04 23:28:10 -0700

    Add better error checker

--- development/applications/falcon_v2/include/falcon_common.h
+++ development/applications/falcon_v2/include/falcon_common.h
@@ -10,8 +10,18 @@
 
 #include "logger.h"
 
+#define FLN_OK   0
+#define FLN_ERR -1
+
 #define DEBUG_LOG(str) logger_write(str)
 
-#define ERR_CHECK(err) do {} while((err) != pdPASS) 
+#define RTOS_ERR_CHECK(err) do {} while((err) != pdPASS)
+
+#define FLN_ERR_CHECK(x) do { \
+  int retval = (x); \
+  if (retval != FLN_OK) { \
+    while(1);\
+  } \
+} while (0)
 
 #endif // FALCON_COMMON_H
