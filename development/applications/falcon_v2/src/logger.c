052b446aeb6ff4d30361153c8031f5fa5c4b995d
Adam Bujak <adam@whisper.ai>
2020-10-04 23:58:18 -0700

    Add mutex to logger

--- development/applications/falcon_v2/src/logger.c
+++ development/applications/falcon_v2/src/logger.c
@@ -2,8 +2,6 @@
 
 #include "bsp.h"
 #include "falcon_common.h"
-#include "stm32f4xx_hal.h"
-#include "stm32412g_discovery.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -11,19 +9,24 @@
 
 fln_uart_handle_t UartHandle;
 
-#define UART_TX_BUFFER_SIZE 128
+#define UART_TX_BUFFER_SIZE  128
+#define MUTEX_RETRY_TICK_CNT 10
 
 typedef struct {
   uint8_t txBuffer[UART_TX_BUFFER_SIZE];
   uint16_t txByteCnt;
   uint16_t txReadIndex;
   uint16_t txWriteIndex;
+
+  SemaphoreHandle_t rxBufferMutex;
 } state_t;
 
 static state_t s;
 
 void logger_write (char *string)
 {
+  while (xSemaphoreTake(s.rxBufferMutex, (TickType_t) MUTEX_RETRY_TICK_CNT) == pdFALSE);
+
   if (s.txByteCnt >= (UART_TX_BUFFER_SIZE - 1)) {
     return;
   }
@@ -40,6 +43,7 @@ void logger_write (char *string)
   }
 
   s.txBuffer[s.txWriteIndex] = '\0';
+  xSemaphoreGive(s.rxBufferMutex);
 }
 
 void logger_task_setup (void)
@@ -47,6 +51,11 @@ void logger_task_setup (void)
   s.txByteCnt = 0;
   s.txWriteIndex = 0;
   s.txReadIndex = 0;
+  s.rxBufferMutex = xSemaphoreCreateMutex();
+
+  if (s.rxBufferMutex == NULL) {
+    error_handler();
+  }
 
   FLN_ERR_CHECK(bsp_uart_init(&UartHandle));
 }
@@ -54,10 +63,15 @@ void logger_task_setup (void)
 void logger_task (void *pvParameters)
 {
   while(1) {
+    if (xSemaphoreTake(s.rxBufferMutex, (TickType_t) MUTEX_RETRY_TICK_CNT) == pdFALSE) {
+      continue;
+    }
+
     if (s.txByteCnt > 0) {
       bsp_uart_write(&UartHandle, &(s.txBuffer[s.txReadIndex]), 1);
       s.txReadIndex= (s.txReadIndex + 1) % UART_TX_BUFFER_SIZE;
       s.txByteCnt--;
     }
+    xSemaphoreGive(s.rxBufferMutex);
   }
 }
