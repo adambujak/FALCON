c89b43908e8a1a6a9ceeeb54b6aa21480fde072d
Adam Bujak <adam@whisper.ai>
2020-10-21 01:07:11 -0700

    Add spi to v2 app and add frf

--- development/applications/falcon_v2/src/main.c
+++ development/applications/falcon_v2/src/main.c
@@ -4,20 +4,22 @@
 #include "leds.h"
 #include "logger.h"
 #include "motors.h"
+#include "device_com.h"
 
 #include <stdint.h>
 
 #define led_TASK_PRIORITY    (tskIDLE_PRIORITY + 2)
 #define logger_TASK_PRIORITY (tskIDLE_PRIORITY + 1)
+#define device_com_TASK_PRIORITY  (tskIDLE_PRIORITY + 3)
 
 int main(void)
 {
+  int32_t taskStatus;
   bsp_board_bringup();
-  leds_task_setup();
   logger_init();
   motors_init();
 
-  int32_t taskStatus;
+  leds_task_setup();
   taskStatus = xTaskCreate(leds_task,
                            "led_task",
                             2*configMINIMAL_STACK_SIZE,
@@ -26,6 +28,19 @@ int main(void)
                             NULL);
 
   RTOS_ERR_CHECK(taskStatus);
+
+  DEBUG_LOG("Falcon V2 Started \r\n");
+
+  device_com_setup();
+  taskStatus = xTaskCreate(device_com_task,
+                        "device_com_task",
+                        4*configMINIMAL_STACK_SIZE,
+                        NULL,
+                        device_com_TASK_PRIORITY,
+                        NULL);
+
+  RTOS_ERR_CHECK(taskStatus);
+
 
   vTaskStartScheduler();
 
