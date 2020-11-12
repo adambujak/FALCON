5286f99eaab590a5f992581f0d722dcb0dc6ffae
Adam Bujak <adam@whisper.ai>
2020-11-11 21:36:15 -0800

    LED ON

--- development/applications/falcon_v2/src/leds.c
+++ development/applications/falcon_v2/src/leds.c
@@ -9,19 +9,48 @@
 
 void leds_toggle(void)
 {
-  bsp_leds_toggle();
+//  bsp_leds_toggle();
+}
+
+volatile uint8_t i = 32;
+uint32_t ledValue = 0x3A0F0F00;
+
+#define LED_LOW() FLN_LED_PORT->BSRR = (uint32_t)FLN_LED_PIN << 16
+#define LED_HIGH() FLN_LED_PORT->BSRR = FLN_LED_PIN
+
+static void callback(void)
+{
+
+  bsp_leds_set(1);
+  bsp_leds_set(0);
+
+  if (ledValue & (1 << i)) {
+	bsp_leds_set(1);
+	bsp_leds_set(0);
+  }
+
+  if (i == 0) {
+    bsp_leds_timer_stop();
+    return;
+  }
+  i--;
+
 }
 
 void leds_task_setup(void)
 {
-  FLN_ERR_CHECK(bsp_leds_init());
+  LED_LOW();
+  for (int i = 0; i < 300; i++);
+  FLN_ERR_CHECK(bsp_leds_init(callback));
+  bsp_leds_timer_start();
+
 }
 
 void leds_task(void *pvParameters)
 {
  while (1) {
 	  DEBUG_LOG("LED Task\r\n");
-    bsp_leds_toggle();
+ //   bsp_leds_toggle();
     vTaskDelay(1500);
   }
 }
