6081349262e11d83fe57be409c3dc512ec5e7180
Devin Bell <dbell44@uwo.ca>
2021-03-23 12:09:10 -0400

    added simple FC mode support to RF stuff, hedwig side sending stuff added but commentented out

--- development/applications/hedwig/include/device_com.h
+++ development/applications/hedwig/include/device_com.h
@@ -2,8 +2,11 @@
 #define DEVICE_COM_H
 
 #include "falcon_common.h"
+// #include "falcon_packet.h"
 
 void device_com_setup(void);
 void device_com_start(void);
+// void device_com_send_packet(void *packet, fp_type_t packetType);
+// void device_com_send_frame(uint8_t *frame);
 
 #endif // DEVICE_COM_H
