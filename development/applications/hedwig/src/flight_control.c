80a0616b105f8efc8a61bfd06e216cc53226f7ab
Devin Bell <dbell44@uwo.ca>
2021-03-23 12:06:45 -0400

    added modes to flight control

--- development/applications/hedwig/src/flight_control.c
+++ development/applications/hedwig/src/flight_control.c
@@ -35,6 +35,7 @@ static SemaphoreHandle_t sensorDataMutex;
 static SemaphoreHandle_t commandDataMutex;
 static SemaphoreHandle_t outputDataMutex;
 
+static fe_falcon_mode_t flight_control_mode = FE_FLIGHT_MODE_IDLE;
 
 static inline BaseType_t lock_sensor_data(void)
 {
@@ -144,12 +145,12 @@ void rt_OneStep(RT_MODEL *const rtM)
   }
 
   /* Get model outputs here */
-  // if(mode == motors_on) {
-  //   motors_set_motor_us(MOTOR_1, rtY_Throttle[0]);
-  //   motors_set_motor_us(MOTOR_2, rtY_Throttle[1]);
-  //   motors_set_motor_us(MOTOR_3, rtY_Throttle[2]);
-  //   motors_set_motor_us(MOTOR_4, rtY_Throttle[3]);
-  // }
+  if(flight_control_mode == FE_FLIGHT_MODE_FLY) {
+    motors_set_motor_us(MOTOR_1, rtY_Throttle[0]);
+    motors_set_motor_us(MOTOR_2, rtY_Throttle[1]);
+    motors_set_motor_us(MOTOR_3, rtY_Throttle[2]);
+    motors_set_motor_us(MOTOR_4, rtY_Throttle[3]);
+  }
 
   /* Indicate task complete */
   OverrunFlag = false;
@@ -202,8 +203,7 @@ void flight_control_set_command_data(fpc_flight_control_t *control_input)
 void flight_control_get_outputs(fpr_status_t *status_response)
 {
   if(lock_output_data() == pdTRUE) {
-    //status_response->status.mode = SomeMode;
-    status_response->status.mode
+    status_response->status.mode = flight_control_mode;
     status_response->status.motor.motor1 = rtY_Throttle[0];
     status_response->status.motor.motor1 = rtY_Throttle[1];
     status_response->status.motor.motor1 = rtY_Throttle[2];
@@ -223,78 +223,123 @@ void flight_control_get_outputs(fpr_status_t *status_response)
   }
 }
 
-void flight_control_start(void)
+void flight_control_set_mode(fe_falcon_mode_t new_mode)
 {
-  FC_timerStatus = xTimerStart( flight_control_timer, 0 );
-  RTOS_ERR_CHECK(FC_timerStatus);
+  switch (new_mode) {
+    case FE_FLIGHT_MODE_IDLE:
+      if (flight_control_mode == FE_FLIGHT_MODE_FCS_READY) {
+        flight_control_mode = FE_FLIGHT_MODE_IDLE;
+      }
+      break;
+    case FE_FLIGHT_MODE_CALIBRATING:
+      if (flight_control_mode <= FE_FLIGHT_MODE_FCS_READY) {
+        flight_control_mode = FE_FLIGHT_MODE_CALIBRATING;
+      }
+      break;
+    case FE_FLIGHT_MODE_FCS_READY:
+      if (flight_control_mode == FE_FLIGHT_MODE_FLY) {
+        if (lock_output_data() == pdTRUE) {
+          motors_set_motor_us(MOTOR_1, 0U);
+          motors_set_motor_us(MOTOR_2, 0U);
+          motors_set_motor_us(MOTOR_3, 0U);
+          motors_set_motor_us(MOTOR_4, 0U);
+          unlock_output_data();
+        }
+        flight_control_mode = FE_FLIGHT_MODE_FCS_READY;
+      }
+      else {
+        flight_control_mode = FE_FLIGHT_MODE_FCS_READY;
+      }
+      break;
+    case FE_FLIGHT_MODE_FLY:
+      if (flight_control_mode == FE_FLIGHT_MODE_FCS_READY) {
+        flight_control_mode = FE_FLIGHT_MODE_FLY;
+      }
+      break;
+  }
 }
 
-// int flight_control_stop(void)
+// void flight_control_mode_rx(uint8_t *data)
 // {
-//   if (somethingSomethingMode != flying) {
-//     FC_timerStatus = xTimerStop( flight_control_timer, 0 );
-//     RTOS_ERR_CHECK(FC_timerStatus);
-//     return 0;
-//   }
-//   else {
-//     LOG_DEBUG("NO STOP WHILE FLYING\r\n");
-//     return 1;
-//   }
+//   fpc_mode_t mode_cmd = {};
+//   fpc_mode_decode(data, &mode_cmd);
+//   flight_control_set_mode(mode_cmd.mode);
+
+//   fpr_mode_t mode_res = {
+//     .mode = flight_control_mode
+//   };
+//   device_com_send_packet(&mode, FPT_MODE_RESPONSE);
 // }
 
-// int flight_control_reset(void)
-// {
-//   if (somethingSomethingMode != flying) {
-//     if (somethingSomethingMode == running) {
-//       flight_control_stop();
-//     }
-
-//     flightController_initialize(rtM, &rtU_Commands, &rtU_Sensors, &rtY_State_Estim, rtY_Throttle);
-//     FC_timerStatus = xTimerReset( flight_control_timer, 0 );
-//     RTOS_ERR_CHECK(FC_timerStatus);
-//     return 0;
-//   }
-//   else {
-//     LOG_DEBUG("NO RESET WHILE FLYING\r\n");
-//     return 1;
-//   }
-// }
+static void flight_control_reset(void)
+{
+  if (flight_control_mode != FE_FLIGHT_MODE_FLY) {
+    if (lock_sensor_data() == pdTRUE ) {
+      if (lock_command_data() == pdTRUE) {
+        if (lock_output_data() == pdTRUE) {
+          flightController_initialize(rtM, &rtU_Commands, &rtU_Sensors, &rtY_State_Estim, rtY_Throttle);
+          unlock_output_data();
+        }
+        else {
+          DEBUG_LOG("outputDataMutex take failed\r\n");
+          error_handler();
+        }
+        unlock_command_data();
+      }
+      else {
+        DEBUG_LOG("commandDataMutex take failed\r\n");
+        error_handler();
+      }
+      unlock_sensor_data();
+    }
+    else {
+      DEBUG_LOG("sensorDataMutex take failed\r\n");
+      error_handler();
+    }
+  }
+  else {
+    DEBUG_LOG("Cannot reset while flying");
+  }  
+}
 
 static void flight_control_task(void *pvParameters)
 {
+  while(flight_control_mode < FE_FLIGHT_MODE_FCS_READY) {
+    vTaskDelay(10);
+  }
 
-  vTaskDelay(10000);
-
-  flight_control_start();
+  FC_timerStatus = xTimerStart( flight_control_timer, 0 );
+  RTOS_ERR_CHECK(FC_timerStatus);
 
   BaseType_t flightTimerNotification;
 
   while(1)
-  {
+  { 
+    if (flight_control_mode >= FE_FLIGHT_MODE_FCS_READY) {
+      /* Wait to be notified of an interrupt. */
+      flightTimerNotification = xTaskNotifyWait(pdFALSE,
+                                           0xFFFFFFFF,
+                                           NULL,
+                                           MS_TO_TICKS(11));
 
-    /* Wait to be notified of an interrupt. */
-    flightTimerNotification = xTaskNotifyWait(pdFALSE,
-                                         0xFFFFFFFF,
-                                         NULL,
-                                         MS_TO_TICKS(10));
+      if (flightTimerNotification == pdPASS) {
 
-    if (flightTimerNotification == pdPASS) {
+        rt_OneStep(rtM);
 
-      rt_OneStep(rtM);
-
-      LOG_DEBUG("z: %7.4f dz: %7.4f yaw, pitch, roll: %7.4f, %7.4f, %7.4f p, q, r: %7.4f, %7.4f, %7.4f\r\n",
-          rtY_State_Estim.z,
-          rtY_State_Estim.dz,
-          rtY_State_Estim.yaw,
-          rtY_State_Estim.pitch,
-          rtY_State_Estim.roll,
-          rtY_State_Estim.p,
-          rtY_State_Estim.q,
-          rtY_State_Estim.r);
-    }
-    else {
-      LOG_DEBUG("timer notif not received\r\n");
-      error_handler();
+        LOG_DEBUG("z: %7.4f dz: %7.4f yaw, pitch, roll: %7.4f, %7.4f, %7.4f p, q, r: %7.4f, %7.4f, %7.4f\r\n",
+            rtY_State_Estim.z,
+            rtY_State_Estim.dz,
+            rtY_State_Estim.yaw,
+            rtY_State_Estim.pitch,
+            rtY_State_Estim.roll,
+            rtY_State_Estim.p,
+            rtY_State_Estim.q,
+            rtY_State_Estim.r);
+      }
+      else {
+        LOG_DEBUG("timer notif not received\r\n");
+        error_handler();
+      }
     }
   }
 }
