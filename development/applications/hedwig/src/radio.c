57c465472511349bb1ba0f41a63407bb502614ec
Adam Bujak <adam@whisper.ai>
2021-04-05 11:42:28 -0700

    add radio fifo

--- development/applications/hedwig/src/radio.c
+++ development/applications/hedwig/src/radio.c
@@ -5,14 +5,63 @@
 #include "frf.h"
 #include "bsp.h"
 
+#define IS_POWER_OF_TWO(num) (((num) & ((num) - 1)) == 0) ? true : false
+
+typedef struct {
+  uint8_t *buffer;
+  uint32_t write_index;
+  uint32_t read_index;
+  uint32_t bytes_available;
+  uint32_t size;
+} fifo_t;
+
+#define RF_RX_BUFFER_SIZE 512
+#define RF_TX_BUFFER_SIZE 512
+
+static uint8_t rx_buffer[RF_RX_BUFFER_SIZE];
+static uint8_t tx_buffer[RF_TX_BUFFER_SIZE];
+
+static fifo_t rx_fifo;
+static fifo_t tx_fifo;
+
 static uint8_t hedwigAddress[RADIO_ADDRESS_LENGTH];
 static uint8_t albusAddress[RADIO_ADDRESS_LENGTH];
 
-uint8_t rx_buffer[32];
-uint8_t tx_buffer[32];
-static volatile bool rfRxReady = false;
-
 static frf_t radio;
+
+static void fifo_init(fifo_t *fifo, uint8_t *buffer, uint32_t size)
+{
+  ASSERT(IS_POWER_OF_TWO(size));
+  fifo->buffer = buffer;
+  fifo->size = size;
+  fifo->write_index = 0;
+  fifo->read_index = 0;
+  fifo->bytes_available = 0;
+}
+
+static void fifo_push(fifo_t *fifo, uint8_t *buffer, uint32_t length)
+{
+  for (uint32_t i = 0; i < length; i++) {
+    fifo->buffer[fifo->write_index] = buffer[i];
+    fifo->write_index = (fifo->write_index + 1) & (fifo->size - 1);
+  }
+
+  fifo->bytes_available = (fifo->bytes_available + length) & (fifo->size - 1);
+}
+
+static int fifo_pop(fifo_t *fifo, uint8_t *dest, uint32_t length)
+{
+  if (length > fifo->bytes_available) {
+    return 0;
+  }
+
+  for (uint32_t i = 0; i < length; i++) {
+    dest[i] = fifo->buffer[fifo->read_index];
+    fifo->read_index = (fifo->read_index + 1) & (fifo->size - 1);
+  }
+  fifo->bytes_available -= length;
+  return length;
+}
 
 static void rf_spi_transfer(void * context, uint8_t * tx_buf, uint16_t tx_len,
                              uint8_t * rx_buf, uint16_t rx_len)
@@ -35,33 +84,38 @@ static void rf_event_callback(frf_event_t event)
       LOG_DEBUG("RF TX SUCCESS\r\n");
       break;
     case FRF_EVENT_RX:
-      rfRxReady = true;
-      frf_getPacket(&radio, rx_buffer);
+    {
+      uint8_t temp[FRF_PACKET_SIZE];
+      frf_getPacket(&radio, temp);
+      fifo_push(&rx_fifo, temp, FRF_PACKET_SIZE);
       LOG_DEBUG("RF RX Event\r\n");
       break;
+    }
   }
 }
 
 uint32_t radio_send_data(uint8_t *source, uint32_t length)
 {
-  frf_sendPacket(&radio, source);
-  return 32;
+  if (length > RF_TX_BUFFER_SIZE) {
+    return 0;
+  }
+
+  fifo_push(&tx_fifo, source, length);
+  return length;
 }
 
 uint32_t radio_get_data(uint8_t *dest, uint32_t length)
 {
-  if (rfRxReady) {
-    memcpy(dest, rx_buffer, 32);
-    rfRxReady = false;
-    return 32;
-  }
-  return 0;
+  return fifo_pop(&rx_fifo, dest, length);
 }
 
 void radio_init(void)
 {
   radio_get_hedwig_address(hedwigAddress);
   radio_get_albus_address(albusAddress);
+
+  fifo_init(&rx_fifo, rx_buffer, RF_RX_BUFFER_SIZE);
+  fifo_init(&tx_fifo, tx_buffer, RF_TX_BUFFER_SIZE);
 
   FLN_ERR_CHECK(bsp_rf_init(rf_isr));
 
@@ -83,4 +137,11 @@ void radio_init(void)
 void radio_process(void)
 {
   frf_process(&radio);
+
+  if (!frf_isSending(&radio)) {
+    uint8_t temp[FRF_PACKET_SIZE];
+    if (fifo_pop(&tx_fifo, temp, FRF_PACKET_SIZE) == FRF_PACKET_SIZE) {
+      frf_sendPacket(&radio, temp);
+    }
+  }
 }
