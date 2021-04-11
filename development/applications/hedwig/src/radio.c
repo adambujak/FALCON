4e91ae8059ba65bf86d15ef57b8e10f43617d791
Adam Bujak <adam@whisper.ai>
2021-04-10 23:43:30 -0700

    add fifo lib to hedwig

--- development/applications/hedwig/src/radio.c
+++ development/applications/hedwig/src/radio.c
@@ -3,17 +3,8 @@
 #include "bsp.h"
 #include "falcon_common.h"
 #include "frf.h"
+#include "fifo.h"
 #include "radio_common.h"
-
-#define IS_POWER_OF_TWO(num) (((num) & ((num) - 1)) == 0) ? true : false
-
-typedef struct {
-  uint8_t *buffer;
-  uint32_t write_index;
-  uint32_t read_index;
-  uint32_t bytes_available;
-  uint32_t size;
-} fifo_t;
 
 #define RF_RX_BUFFER_SIZE 512
 #define RF_TX_BUFFER_SIZE 512
@@ -28,40 +19,6 @@ static uint8_t hedwig_address[RADIO_ADDRESS_LENGTH];
 static uint8_t albus_address[RADIO_ADDRESS_LENGTH];
 
 static frf_t radio;
-
-static void fifo_init(fifo_t *fifo, uint8_t *buffer, uint32_t size)
-{
-  ASSERT(IS_POWER_OF_TWO(size));
-  fifo->buffer = buffer;
-  fifo->size = size;
-  fifo->write_index = 0;
-  fifo->read_index = 0;
-  fifo->bytes_available = 0;
-}
-
-static void fifo_push(fifo_t *fifo, uint8_t *buffer, uint32_t length)
-{
-  for (uint32_t i = 0; i < length; i++) {
-    fifo->buffer[fifo->write_index] = buffer[i];
-    fifo->write_index = (fifo->write_index + 1) & (fifo->size - 1);
-  }
-
-  fifo->bytes_available = (fifo->bytes_available + length) & (fifo->size - 1);
-}
-
-static int fifo_pop(fifo_t *fifo, uint8_t *dest, uint32_t length)
-{
-  if (length > fifo->bytes_available) {
-    return 0;
-  }
-
-  for (uint32_t i = 0; i < length; i++) {
-    dest[i] = fifo->buffer[fifo->read_index];
-    fifo->read_index = (fifo->read_index + 1) & (fifo->size - 1);
-  }
-  fifo->bytes_available -= length;
-  return length;
-}
 
 static void rf_spi_transfer(void *context, uint8_t *tx_buf, uint16_t tx_len, uint8_t *rx_buf, uint16_t rx_len)
 {
