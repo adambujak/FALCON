3890555ce2c86b95864e098aaa08b80555be8666
Adam Bujak <adamjbujak@gmail.com>
2021-04-11 16:22:50 -0700

    Add packet queuing to hedwig (#48)
    * add packet queuing to hedwig
    
    * change timer and add header
    
    Co-authored-by: Adam Bujak <adam@whisper.ai>

--- development/applications/hedwig/include/board.h
+++ development/applications/hedwig/include/board.h
@@ -12,8 +12,8 @@
 #define SYSCLK_FREQ                           96000000
 
 /* System Time */
-#define SYSTEM_TIME_TIMER                     TIM3
-#define SYSTEM_TIME_CLK_EN()                  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM3)
+#define SYSTEM_TIME_TIMER                     TIM2
+#define SYSTEM_TIME_CLK_EN()                  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2)
 
 /* LEDS */
 #ifdef BOARD_V1
