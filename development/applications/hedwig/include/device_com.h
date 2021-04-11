842895227718b98411943e98128dbd469aaf3e38
Devin Bell <47790946+devinbellll@users.noreply.github.com>
2021-04-11 16:29:29 -0400

    Update development/applications/hedwig/include/device_com.h
    Co-authored-by: Adam Bujak <adamjbujak@gmail.com>

--- development/applications/hedwig/include/device_com.h
+++ development/applications/hedwig/include/device_com.h
@@ -5,7 +5,5 @@
 
 void device_com_setup(void);
 void device_com_start(void);
-// void device_com_send_packet(void *packet, fp_type_t packetType);
-// void device_com_send_frame(uint8_t *frame);
 
 #endif // DEVICE_COM_H
