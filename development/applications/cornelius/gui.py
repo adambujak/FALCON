1c19b96384ad54aba30c2a8e4bf73a7b5332a2cb
Devin Bell <dbell44@uwo.ca>
2021-06-03 12:49:19 -0400

    made it nice with spin boxes

--- development/applications/cornelius/gui.py
+++ development/applications/cornelius/gui.py
@@ -3,28 +3,33 @@ import signal
 import sys
 import PySimpleGUIQt as sg
 
+import numpy as np
+
 def handle_button_event(event):
-    if event == 'test-query':
-        print ('test-query')
+    if event == 'Test-Query':
+        serial_monitor_print('test-query sent')
 
-    if event == 'radio-stats':
-        print ('radio')
+    if event == 'Radio-Stats':
+        serial_monitor_print('radio-stats query sent')
 
-    if event == 'idle':
-        print ('idle')
+    if event == 'Idle':
+        serial_monitor_print('Idle Mode Command')
 
-    if event == 'calibrate':
-        print ('calibrate')
+    if event == 'Calibrate':
+        serial_monitor_print('Calibrate Command')
 
-    if event == 'ready':
-        print ('ready')
+    if event == 'Ready':
+        serial_monitor_print('Ready Mode Command')
 
-    if event == 'fly':
-        print ('fly')
+    if event == 'Fly':
+        serial_monitor_print('Fly Mode Command')
 
-    if event == 'yaw-submit':
-        yaw_kp = window['yaw-kp']
-        serial_monitor_print('yaw value:' + str(yaw_kp.get()))
+    if event == 'Attitude Submit':
+        serial_monitor_print('Attitude values set to: {}, {}, {}'.format(values['attitude-kp'],values['attitude-ki'],values['attitude-kd']))
+    if event == 'Altitude Submit':
+        serial_monitor_print('Altitude values set to: {}, {}, {}'.format(values['altitude-hover'],values['altitude-kp'],values['altitude-kd']))
+    if event == 'Yaw Submit':
+        serial_monitor_print('Yaw values set to: {}, {}'.format(values['yaw-kp'],values['yaw-kd']))
 
 def signal_handler(sig, frame):
     sys.exit(0)
@@ -35,30 +40,40 @@ def serial_monitor_print(text):
 
 signal.signal(signal.SIGINT, signal_handler)
 
-multiline = sg.Multiline(size=(30, 5), key='serial-monitor')
+
+# Serial Monitor
+serial_monitor = sg.Multiline(size=(50, 10), key='serial-monitor')
+
+# Spin Boxes
+att_kp_spin = sg.Spin(key='attitude-kp',size=(5,1),values=np.arange(0.00,10.00,0.01), initial_value=3.60)
+att_ki_spin = sg.Spin(key='attitude-ki',size=(5,1),values=np.arange(0.00,10.00,0.01), initial_value=0.20)
+att_kd_spin = sg.Spin(key='attitude-kd',size=(5,1),values=np.arange(0.00,10.00,0.01), initial_value=0.18)
+alt_kp_spin = sg.Spin(key='altitude-kp',size=(5,1),values=np.arange(0.00,10.00,0.01), initial_value=0.00)
+alt_kd_spin = sg.Spin(key='altitude-kd',size=(5,1),values=np.arange(0.00,10.00,0.01), initial_value=0.00)
+alt_hover_spin = sg.Spin(key='altitude-hover',size=(5,1),values=np.arange(0.00,2.00,0.01), initial_value=1.00)
+yaw_kp_spin = sg.Spin(key='yaw-kp',size=(5,1),values=np.arange(0.00,10.00,0.01), initial_value=0.00)
+yaw_kd_spin = sg.Spin(key='yaw-kd',size=(5,1),values=np.arange(0.00,10.00,0.01), initial_value=0.00)
 
 layout = [ [sg.Text('Cornelius')],
-           [sg.Button('test-query')],
-           [sg.Button('radio-stats')],
-           [sg.Button('idle')],
-           [sg.Button('calibrate')],
-           [sg.Button('ready')],
-           [sg.Button('fly')],
-           [sg.Text('attitude control')],
-           [sg.Text('kp'), sg.Input(key='attitude-kp'), sg.Text('ki'), sg.Input(key='attitude-ki'), sg.Text('kd'), sg.Input(key='attitude-kd')],
-           [sg.Button('attitude-submit')],
-           [sg.Text('altitude control')],
-           [sg.Text('hover'), sg.Input(key='altitude-hover'), sg.Text('kp'), sg.Input(key='altitude-kp'), sg.Text('kd'), sg.Input(key='altitude-kd')],
-           [sg.Button('altitude-submit')],
-           [sg.Text('yaw control')],
-           [sg.Text('kp'), sg.Input(key='yaw-kp'), sg.Text('kd'), sg.Input(key='yaw-kd')],
-           [sg.Button('yaw-submit')],
-           [multiline]
+           [sg.Button('Test-Query'),sg.Button('Radio-Stats')],
+           [sg.Button('Idle',size=(9,1))],
+           [sg.Button('Calibrate',size=(9,1))],
+           [sg.Button('Ready',size=(9,1))],
+           [sg.Button('Fly',size=(9,1))],
+           [sg.Text('Attitude Control')],
+           [sg.Text('Kp'), att_kp_spin, sg.Text('Ki'), att_ki_spin, sg.Text('Kd'), att_kd_spin],
+           [sg.Button('Attitude Submit',size=(9,1))],
+           [sg.Text('Altitude Control')], 
+           [sg.Text('Hover'), alt_hover_spin, sg.Text('Kp'), alt_kp_spin, sg.Text('kd'), alt_kd_spin],
+           [sg.Button('Altitude Submit',size=(9,1))],
+           [sg.Text('Yaw Control')],
+           [sg.Text('kp'), yaw_kp_spin, sg.Text('kd'), yaw_kd_spin],
+           [sg.Button('Yaw Submit',size=(9,1))],
+           [serial_monitor]
          ]
 
 # Create the window
 window = sg.Window("Cornelius", layout)
-print(window)
 
 # Create an event loop
 while True:
@@ -70,6 +85,3 @@ while True:
         break
 
     handle_button_event(event)
-
-    serial_monitor_print("hello")
-
